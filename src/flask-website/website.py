a9b06d2556ad17ec53b220af81ec93cf2ed8f08e
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-26 10:07:24 +0000

    Page.html + route -> Connexion + Information utilisateur

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def start(): 
     return render_template('start.html')
-    
+
+@app.route("/connect")
+def connect():
+    return render_template('connect.html')
+
+@app.route("/permit")
+def permit():
+    return render_template('UserPermit.html')
+
+@app.route("/info")
+def info():
+    return render_template ('UserInfo')
