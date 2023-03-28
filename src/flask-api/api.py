b577bfbd3183a6f70907e08c628fc22dc3144580
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-28 14:10:03 +0000

    Debut api.py

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import request
+import json
+import mysql.connector
+from db import mydb
 
 app = Flask(__name__)
 
@@ -6,3 +10,25 @@ app = Flask(__name__)
 def hello():
     return "Hello, API!",met
 
+
+@app.route('/api/receive_data',methods=['POST'])
+def receive_data():
+    data = request.json
+    send_pswd_user_sql(data)
+    return 'Je viens de api'
+
+    def send_pswd_user_sql(data):
+        login=[]
+        login.insert(0,data[0])
+        pswd = data[1]
+        cursor = mydb.cursor()
+        cursor.executee('SELECT * FROM auth WHERE = %s', (login))
+        rows = cursor.fetchall()
+        print(rows)
+        result=[]
+        for row in rows:
+            result.append({'auth': row[0], 'password': row[1]})
+            if(result[0]['password'] == pswd):
+                return True
+            else:
+                return False
