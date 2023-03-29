345a1f991245757d381f50f4d95d540a63dca343
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-29 21:06:14 +0000

    Ajout de la connexion à la DataBase

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -8,29 +8,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, API!",met
+    return "Hello, API!"
 
-
-@app.route('/api/receive_data',methods=['POST'])
-def receive_data():
-    data = request.json
-    send_pswd_user_sql(data)
-    return 'Je viens de api'
-
-    def send_pswd_user_sql(data):
-        login=[]
-        login.insert(0,data[0])
-        pswd = data[1]
-        cursor = mydb.cursor()
-        cursor.executee('SELECT * FROM auth WHERE = %s', (login))
-        rows = cursor.fetchall()
-        print(rows)
-        result=[]
-        for row in rows:
-            result.append({'auth': row[0], 'password': row[1]})
-            if(result[0]['password'] == pswd):
-                return True
-            else:
-                return False
-
-    
--- src/flask-api/mydb.py
+++ src/flask-api/mydb.py
@@ -1,12 +1,18 @@
-"""
-import mysql.connector
+from flask import Flask
+from mysql.connector import DataBase, InterfaceError, connect
 
-# Configuration de la connexion à la bdd
-mydb = mysql.connector.connect(host="localhost", user="root", password="dangreaj", database="identity")
-if mydb.is_connected():
-    print("Conenxion à la Base de Donnée reussi")
-else:
-    print("Impossible de se connecter à la base de donnée")
-
-# Ajout d'un utilisateur de bdd
-"""
+## Connect to DataBase
+def connexion(host: str, database: str, login: str, pwd: str):
+    ## Connection Data
+    try:
+        cnx = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=pwd,
+            database=database,
+        )
+        print(f"La connexion à la base de donnée est effectuée")
+        return cnx
+    ## Si erreur de connexion passe ici
+    except mysql.connector.Error as err:
+        print (f"Connexion de connexion à la base de données: {err}")
