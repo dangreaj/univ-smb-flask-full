5955afcb4fcf39ceae1a62beaba6521119910e52
Jeremy DANGREAUX <dangreaj@gmail.com>
2023-03-30 13:06:39 +0000

    début d'auth

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-import json
+import jsonify
 import mysql.connector
 from db import mydb
 
@@ -10,3 +10,8 @@ app = Flask(__name__)
 def hello():
     return "Hello, API!"
 
+### Reverra les donnée JSON
+@app.route("/api/log",method=['GET'])
+def get_data():
+    sql = "SELECT id_user,login, password FROM identity.auth"
+    
--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -2,19 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+## URL api.py
+url = 'http://127.0.0.1:5001/api'
+
 @app.route("/")
 def start(): 
+    login = request.form['login']
+    passwd = request.form['passwd']
+    data = list([login,passswd])
+    response = request.post(url + '/log', json=data)
+
     return render_template('connect.html')
 
 @app.route("/index", methods=['POST'])
 def index():
-    login = request.form['login']
-    passsword = request.form['password']
-    donnees = list([login,passsword])
-    response = requests.post('http://127.0.0.1:5001/api/recevoir-donnees',json=donnees)
-    if response.ok:         #print(response) -> <Response [200]>
-        return render_template('start.html')
-    else:
         return render_template('start.html')
 
 @app.route("/permit")
